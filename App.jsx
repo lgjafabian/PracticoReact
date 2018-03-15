4c7b917272abde6d96aabf83e6776c853e0d8558
lgjafabian <lgjafabian@gmail.com>
2018-03-15 11:33:00 -0300

    Estructura del projecto

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hola ITAcademy!
+         </div>
+      );
+   }
+}
+
+export default App;
