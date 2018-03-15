ac6a75d0a5282b48ad26f7627f7e7a907b9fcf1b
lgjafabian <lgjafabian@gmail.com>
2018-03-15 11:55:45 -0300

    valores test

--- App.jsx
+++ App.jsx
@@ -7,15 +7,21 @@ class App extends React.Component {
         this.state = {
            data: 
            [
-              {id:'1', nombre:'a', url:'A'},
-              {id:'2', nombre:'b', url:'B'},
-              {id:'3', nombre:'c', url:'C'},
-              {id:'4', nombre:'d', url:'D'},
-              {id:'5', nombre:'e', url:'E'},
-              {id:'6', nombre:'f', url:'F'},
-              {id:'7', nombre:'g', url:'G'},
-              {id:'8', nombre:'h', url:'H'},
-              {id:'9', nombre:'i', url:'I'}
+                [
+                    {id:'1', name:'a', url:'A'},
+                    {id:'2', name:'b', url:'B'},
+                    {id:'3', name:'c', url:'C'}
+                ],
+                [
+                    {id:'4', name:'d', url:'D'},
+                    {id:'5', name:'e', url:'E'},
+                    {id:'6', name:'f', url:'F'}
+                ],
+                [
+                    {id:'7', name:'g', url:'G'},
+                    {id:'8', name:'h', url:'H'},
+                    {id:'9', name:'i', url:'I'}
+                ]
            ]
         }
      }
@@ -23,10 +29,30 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                Hola ITAcademy!
-            </div>
+            <table>
+               <tbody>
+                  {
+                        this.state.data.map((row, i) => 
+                              <TableRow key = {i} row = {row} />
+                        )
+                  }
+               </tbody>
+            </table>
+         </div>
         );
     }
 }
+
+class TableRow extends React.Component {
+    render() {
+       return (
+          <tr>
+             <td>{this.props.row[0].name}</td>
+             <td>{this.props.row[1].name}</td>
+             <td>{this.props.row[2].name}</td>
+          </tr>
+       );
+    }
+ }
 
 export default App;
